cee499d983060b757e10155a4cd2adfbcb5b7e32
tomcatbaluwaris <edvveenhuis@gmail.com>
2022-08-05 14:57:45 +0200

    Simple Pattern and RIP- Pattern => Replace if with polymorphism

--- Factory/FactoryCustomer.cs
+++ Factory/FactoryCustomer.cs
@@ -1,19 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Dynamic;
 using MiddleLayer;
 
 namespace Factory
 {
-    public static class FactoryCustomer
+    public static class FactoryCustomer // Simple Factory Pattern
     {
+        private static Dictionary<string, CustomerBase> customers = new Dictionary<string, CustomerBase>();
+
+        //Design Pattern RIP Pattern => Replace if with polymorphismn
+        static FactoryCustomer()
+        {
+            customers.Add("Customer",new Customer());
+            customers.Add("Lead",new Lead());
+        }
         public static CustomerBase Create(string typeCustomer)
         {
-            if (typeCustomer == "Customer")
-            {
-                return new Customer();
-            }
+            var customerBase = customers[typeCustomer];
+            return customerBase;
 
-            return new Lead();
         }
     }
 }
